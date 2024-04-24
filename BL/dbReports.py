c3613341d284081933d1bb75ce4fcc4ac415abb7
saipa <saipavan861@gmail.com>
2024-04-24 12:46:04 +0530

    reports errors fixed

--- BL/dbReports.py
+++ BL/dbReports.py
@@ -16,15 +16,30 @@ class ReportsBL:
             rows = sqlobj.execstoredproc(sqlquery)
             if rows == None or len(rows) == 0:
                 return cutil.InvalidResult('No data available').__dict__
-
             # Convert datetime objects to strings only if not None
             for item in rows:
-                if 'Date' in item and item['Date'] is not None:
-                    item['Date'] = item['Date'].strftime('%Y-%m-%d')
-                if 'In Time' in item and item['In Time'] is not None:
-                    item['In Time'] = item['In Time'].strftime('%H:%M:%S')
-                if 'Out Time' in item and item['Out Time'] is not None:
-                    item['Out Time'] = item['Out Time'].strftime('%H:%M:%S')
+                # if 'Date' in item and item['Date'] is not None and not isinstance(item['Date'], str):
+                #     item['Date'] = item['Date'].strftime('%Y-%m-%d')
+                # if 'In Time' in item and item['In Time'] is not None and not isinstance(item['In Time'], str):
+                #     item['In Time'] = item['In Time'].strftime('%H:%M:%S')
+                # if 'Out Time' in item and item['Out Time'] is not None and not isinstance(item['Out Time'], str):
+                #     item['Out Time'] = item['Out Time'].strftime('%H:%M:%S')
+                #
+                # if 'Transaction Date' in item and item['Transaction Date'] is not None and not isinstance(item['Transaction Date'], str):
+                #     item['Transaction Date'] = item['Transaction Date'].strftime('%Y-%m-%d %H:%M:%S')
+
+                if 'Date' in item and item['Date'] is not None :
+                    item['Date'] = str(item['Date'])
+                if 'In Time' in item and item['In Time'] is not None :
+                    item['In Time'] = str(item['In Time'])
+                if 'Out Time' in item and item['Out Time'] is not None :
+                    item['Out Time'] = str(item['Out Time'])
+
+                if 'Transaction Date' in item and item['Transaction Date'] is not None :
+                    item['Transaction Date'] = str(item['Transaction Date'])
+
+                if 'Duration' in item and item['Duration'] is not None:
+                    item['Duration'] = str(item['Duration'])
 
             resultmodel =  dataoutputmodel.DataOutputModel('getReports',rows,True)
             return resultmodel.__dict__

3cb7b99b74232014bed38006ea024852a66616f2
saipa <saipavan861@gmail.com>
2024-04-27 15:39:27 +0530

    addmobileaccess,portal access

---
+++ Services/Access.py
@@ -0,0 +1,19 @@
+from flask import Blueprint,request,jsonify
+from BL import dbAccess
+from HelperClass.commonutil import commonutil
+
+Access = Blueprint('Access',__name__)
+
+@Access.route('/addmobileaccess',methods = ['Post'])
+def addmobileaccess():
+    data = request.json
+    accbl = dbAccess.AccessBL()
+    result = accbl.dbAddMobileAccess(data)
+    return jsonify(result)
+
+@Access.route('/addempportalaccess',methods = ['Post'])
+def createbranch():
+    data = request.json
+    accbl = dbAccess.AccessBL()
+    result = accbl.dbAddEmpPortalAccess(data)
+    return jsonify(result)

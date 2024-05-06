ca8bd351f4a1121c902438b2c6ff25554bead1c5
saipa <saipavan861@gmail.com>
2024-05-06 15:03:38 +0530

    model

--- HelperClass/dataoutputmodel.py
+++ HelperClass/dataoutputmodel.py
@@ -1,10 +1,12 @@
 
 class DataOutputModel:
-    def __init__(self, message, resultdata, status):
+    def __init__(self, message, resultdata, status,popuptype=False):
         self.Message = message
         self.ResultData = resultdata
         self.Status = status
+        self.PopupType = popuptype
     
     Message = ""
     ResultData=""
     Status=""
+    PopupType=""
